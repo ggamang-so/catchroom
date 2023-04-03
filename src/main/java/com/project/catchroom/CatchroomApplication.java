b5e03c2052857422a4e9fd578d8c83cdc3ed6e88
ggamangso1 <ggamangso@gmail.com>
2023-04-03 19:39:09 +0900

    first commit catchroom project

---
+++ src/main/java/com/project/catchroom/CatchroomApplication.java
@@ -0,0 +1,13 @@
+package com.project.catchroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/catchroom/CatchroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.catchroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
