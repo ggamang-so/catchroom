03cedf0dbed7770fc70290170a9005b21ee35c4a
ggamangso1 <ggamangso@gmail.com>
2023-04-06 22:03:32 +0900

    MemberDTO setting, lombok install

--- src/main/java/com/project/catchroom/dto/MemberDTO.java
+++ src/main/java/com/project/catchroom/dto/MemberDTO.java
@@ -1,4 +1,21 @@
 package com.project.catchroom.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class MemberDTO {
+    private int no;
+    private String userid;
+    private String username;
+    private String tel;
+    private int zipcode;
+    private String address;
+    private String addrDetail;
+    private String joinDate;
+    private String blackUser;
+
 }
